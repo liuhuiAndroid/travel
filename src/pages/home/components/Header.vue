fdcfa91bb496d92976055547da1ab5dba65e9aa3
liuhui <642404044@qq.com>
2018-06-05 20:44:27 +0800

    city router

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -7,10 +7,12 @@
        <span class="iconfont">&#xe632;</span>
        输入城市/景点/游玩主题
     </div>
-   <div class="header-right">
-       {{this.city}}
-       <span class="iconfont arrow-icon">&#xe64a;</span>
-    </div>
+    <router-link to="/city">
+        <div class="header-right">
+            {{this.city}}
+            <span class="iconfont arrow-icon">&#xe64a;</span>
+        </div>
+    </router-link>
 </div>
 </template>
 
@@ -27,43 +29,46 @@ export default {
 // scoped使样式不对其他组件产生影响
 <style lang="stylus" scoped>
 // 1rem = html font-size = 50px
-@import '~styles/varibles.styl'
+@import '~styles/varibles.styl';
+
 .header {
-    display: flex
+    display: flex;
     // ??
-    line-height: 0.86rem
-    background: $bgColor
+    line-height: $headerHeight
+    background: $bgColor;
     // 字体颜色
-    color: #fff
+    color: #fff;
 
     .header-left {
-        width: 0.64rem
-        float: left
-        .back-icon{
-            text-align: center
-            font-size: .4rem
+        width: 0.64rem;
+        float: left;
+
+        .back-icon {
+            text-align: center;
+            font-size: 0.4rem;
         }
     }
 
     .header-input {
-        flex: 1
-        height: .62rem
-        line-height: .62rem
-        margin-top: .12rem
-        margin-left: .2rem
-        padding-left: .2rem
-        background: #fff
-        border-radius: .1rem
-        color: #ccc
+        flex: 1;
+        height: 0.62rem;
+        line-height: 0.62rem;
+        margin-top: 0.12rem;
+        margin-left: 0.2rem;
+        padding-left: 0.2rem;
+        background: #fff;
+        border-radius: 0.1rem;
+        color: #ccc;
     }
 
-    .header-right{
-        width: 1.24rem
-        float: right
-        text-align: center
-        .arrow-icon{
-            font-size: .24rem
-            margin-left: -.04rem
+    .header-right {
+        width: 1.24rem;
+        float: right;
+        text-align: center;
+        color: #fff
+        .arrow-icon {
+            font-size: 0.24rem;
+            margin-left: -0.04rem;
         }
     }
 }
